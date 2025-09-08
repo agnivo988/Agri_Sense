2d03fd21fd37a4ab5c293edbd50b7b85d77bf4cd
agnivo988 <agnivamukherjee977@gmail.com>
2025-09-08 22:43:48 +0530

    issues fixed

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-import { cookies } from "next/headers"; // for server access to cookies
+
 import 'leaflet/dist/leaflet.css';
 
  // import your i18n config
